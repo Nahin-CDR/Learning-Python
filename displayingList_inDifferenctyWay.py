d68f058b9cb1d7d43a4372ef3c00f14ed0119757
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2021-07-25 23:21:40 +0600

    Create displayingList_inDifferenctyWay.py

---
+++ displayingList_inDifferenctyWay.py
@@ -0,0 +1,10 @@
+# Nahin The Coder
+# Date : 25 - 07 - 2021
+
+
+print("Printing a List in different different ways : ")
+myList = [1, 3, 4, 5, 5, 6, 6, 66, 3]
+print(myList)
+print(*myList)
+print(*myList, sep=",")
+print(*myList, sep="\n")
